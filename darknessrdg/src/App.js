dd3d48e02c7317ad4fcf973c952df6811b16e3c5
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-23 02:23:50 -0300

    add helper.css

--- darknessrdg/src/App.js
+++ darknessrdg/src/App.js
@@ -7,6 +7,7 @@ import '../node_modules/materialize-css/dist/css/materialize.min.css'
 import '../node_modules/material-icons/iconfont/material-icons.css'
 
 import "./styles.css"
+import "./helper.css"
 
 
 import Routes from './routes/routes'
