5c022a755a2bdcd636fe46b906380aa01b1eb35a
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-23 03:58:51 -0300

    add mais elementos ao banner

--- darknessrdg/src/components/Banner/index.js
+++ darknessrdg/src/components/Banner/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './styles.css'
 
 import BannerImg from '../../assets/banner.jpeg'
 
@@ -6,7 +7,8 @@ import BannerImg from '../../assets/banner.jpeg'
 const ADJETIVOS = [
     'Web Developer',
     'Competitive Programmer',
-    'Python lover',
+    'Gamer',
+    'Python Lover',
     'Judoka'
 ]
 
@@ -29,7 +31,7 @@ export default () => {
     const [adjetivoAtual, setAdjetivoAtual] = useState(ADJETIVOS[0])
     const [palavra, setPalavra] = useState('')
 
-    
+
 
     async function apagarPalavra() {
         while (indexPalavra >= 0) {
@@ -38,21 +40,21 @@ export default () => {
         }
 
         indexPalavra = 0
-        indexAdjetivo =  indexAdjetivo === ADJETIVOS.length - 1 ? 0 : indexAdjetivo + 1   
-        
+        indexAdjetivo = indexAdjetivo === ADJETIVOS.length - 1 ? 0 : indexAdjetivo + 1
+
         setAdjetivoAtual(ADJETIVOS[indexAdjetivo])
-        
+
         await novoTempo(tempoEspera)
-        
+
     }
 
     async function digitarNovaPalavra() {
         while (indexPalavra <= adjetivoAtual.length) {
-            
+
             setPalavra(adjetivoAtual.slice(0, indexPalavra++))
             await novoTempo(tempoDigitar)
         }
-        
+
         await novoTempo(tempoEspera)
         apagarPalavra()
     }
@@ -65,8 +67,33 @@ export default () => {
     return (
         <div id="banner" className="bg-cover" style={{ 'backgroundImage': `url(${BannerImg})` }}>
             <div className="black-cover">
-                <div className="h-vh v-aling-center">
-                    <h1 className="m-0 white-text">Eu sou, {palavra}<span>|</span></h1>
+                <div className="h-vh v-aling-center container">
+                    <div className="row center">
+                        <h2 className="m-0 white-text bol">Eu sou {palavra}<span>|</span></h2>
+                        <h6 className="grey-text text-lighten-1 mt-4">
+                            Gamer, judoka, violonista por paixão e
+                        </h6>
+                        <h6 className="grey-text text-lighten-1">
+                            <strong className="bold">Progamador</strong> nas horas vagas.
+                        </h6>
+                        <div className="links-content">
+                            <span>
+                                <a href="https://github.com/DarknessRdg/" className="white-text">
+                                    <i className="fab fa-github"></i>
+                                </a>
+                            </span>
+                            <span>
+                                <a href="https://www.facebook.com/luan.dasilva.925" className="white-text">
+                                    <i className="fab fa-facebook-f"></i>
+                                </a>
+                            </span>
+                            <span>
+                                <a href="https://www.instagram.com/luan_26/" className="white-text">
+                                    <i className="fab fa-instagram"></i>
+                                </a>
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
