0f0c0adf9ccc9b0024d73aa7a7f2bd762d0363c8
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-23 02:25:10 -0300

    nav fixed

--- darknessrdg/src/components/Nav/index.js
+++ darknessrdg/src/components/Nav/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './styles.css'
 
 
 export default () =>
-    <nav className="grey lighten-5">
+    <nav className="nav-transparent">
         <div className="nav-wrapper navbar container">
-            <a href="#!" className="brand-logo black-text">Luan</a>
+            <a href="#!" className="brand-logo ">Luan</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#sobre" className="black-text">Sobre</a></li>
-                <li><a href="#portifolio" className="black-text">Portifólio</a></li>
-                <li><a href="#!" className="black-text">Contato</a></li>
+                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#portifolio">Portifólio</a></li>
+                <li><a href="#!">Contato</a></li>
             </ul>
         </div>
     </nav>
