bfa5a854c81eaa3b4e7dad2fa2febc0b69626731
luan rodrigues <luanrodrigues007@hotmail.com>
2020-01-24 18:16:19 -0300

    scrollspsy

--- darknessrdg/src/components/Nav/index.js
+++ darknessrdg/src/components/Nav/index.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './styles.css'
 
+import scrollspsy from '../../utils/scrollspy'
 
-export default () =>
-    <nav className="nav-transparent">
-        <div className="nav-wrapper navbar container">
-            <a href="#!" className="brand-logo ">Luan</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#sobre">Sobre</a></li>
-                <li><a href="#portifolio">Portifólio</a></li>
-                <li><a href="#!">Contato</a></li>
-            </ul>
-        </div>
-    </nav>
+export default () => {
+    useEffect(() => {
+        scrollspsy()
+    }, [])
+    return (
+        <nav className="nav-transparent scrollspsy-content">
+            <div className="nav-wrapper navbar container">
+                <a href="#!" className="brand-logo" data-target="banner">Luan</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="#sobre" data-target="sobre">Sobre</a></li>
+                    <li><a href="#portifolio">Portifólio</a></li>
+                    <li><a href="#!">Contato</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
