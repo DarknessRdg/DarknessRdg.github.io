3ad5911b1c6b978613498a9af469fdc6ab0c2701
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-26 03:00:08 -0300

    arrumar responsividade da imagem

--- darknessrdg/src/components/Sobre/Esquerda/index.js
+++ darknessrdg/src/components/Sobre/Esquerda/index.js
@@ -7,7 +7,7 @@ import ImagemPerfil from '../../../assets/me.jpg'
 export default () => {
 
     return (
-        <div className="about-me-img-content">
+        <div className="about-me-img-content left-align">
             <img src={ImagemPerfil} alt="Luan Rodrigues"/>
         </div>
     )
