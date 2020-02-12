add4e0d5a8b4023aeee8b5b04dccc9596ce03498
luan rodrigues <luanrodrigues007@hotmail.com>
2020-02-12 09:24:41 -0300

    refactor

--- darknessrdg/src/components/Section/index.js
+++ darknessrdg/src/components/Section/index.js
@@ -1,18 +1,13 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
-export default (props) => (
-
-    <div id={props.id} className="section pb-0">
-        <div className="container">
-            <div className="d-block center mb-5">
-                <h3 className="center bold section-title"><span>{props.title}</span></h3>
-            </div>
-
-            <div className="center">
-                {props.body}
-            </div>
-
-        </div>
+export default props => (
+  <div id={props.id} className="section pb-0">
+    <div className="d-block center mb-5">
+      <h3 className="center bold section-title">
+        <span>{props.title}</span>
+      </h3>
     </div>
-)
+    <div className="center">{props.body}</div>
+  </div>
+);
--- darknessrdg/src/components/Sobre/index.js
+++ darknessrdg/src/components/Sobre/index.js
@@ -1,30 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import Section from '../Section'
-import Esquerda from './Esquerda'
-import Direita from './Direita'
-
+import Section from "../Section";
+import Esquerda from "./Esquerda";
+import Direita from "./Direita";
 
 const Conteudo = () => {
-    return (
-        <div className="row mb-0">
-            <div className="col m12 l6">
-                <Esquerda />
-            </div>
-
-            <div className="col m12 l6">
-                <Direita />
-            </div>
+  return (
+    <div className="container">
+      <div className="row mb-0">
+        <div className="col m12 l6">
+          <Esquerda />
         </div>
-    )
-}
+
+        <div className="col m12 l6">
+          <Direita />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default () => {
-    return (
-        <Section
-            id="sobre"
-            title="Sobre mim"
-            body={<Conteudo />}
-        />
-    )
-}
+  return <Section id="sobre" title="Sobre mim" body={<Conteudo />} />;
+};
