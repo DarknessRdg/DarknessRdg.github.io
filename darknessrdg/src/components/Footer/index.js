aa0ab685bb607cafb30d1899315d929de68d42e0
luan rodrigues <luanrodrigues007@hotmail.com>
2020-02-13 11:43:49 -0300

    init footer

---
+++ darknessrdg/src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <div className="center pt-4 pb-3">
+    <h7 className="grey-text text-darken-2">
+      Web site feito com <i class="fas fa-heart"></i> e ReactJS
+    </h7>
+  </div>
+);
