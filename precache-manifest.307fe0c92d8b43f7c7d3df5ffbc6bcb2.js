6aa2df0c41c95324d486fc5f6f958ad1022e4d40
DarknessRdg <DarknessRdg@users.noreply.github.com>
2022-03-28 03:48:21 +0000

    deploy: f5c35ba823c0a3bd1a1da5a21c4c652a8e3de1f6

---
+++ precache-manifest.307fe0c92d8b43f7c7d3df5ffbc6bcb2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fbd418974ef327c8a8b548d03ec48c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff48df44cf0a9096ee2",
+    "url": "/static/css/2.a8bd4d35.chunk.css"
+  },
+  {
+    "revision": "3e7e9e3f6364966bb9cc",
+    "url": "/static/css/main.b3e39ac7.chunk.css"
+  },
+  {
+    "revision": "aff48df44cf0a9096ee2",
+    "url": "/static/js/2.f9f03b4a.chunk.js"
+  },
+  {
+    "revision": "2dc93012b403773b61d0165a695aba23",
+    "url": "/static/js/2.f9f03b4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e7e9e3f6364966bb9cc",
+    "url": "/static/js/main.f612c294.chunk.js"
+  },
+  {
+    "revision": "b79d75fd4bc54d53c002",
+    "url": "/static/js/runtime-main.23c8ef9d.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
